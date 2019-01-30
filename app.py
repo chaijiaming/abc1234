43434bcd3d9178b34c5376d7ad2971d564e05d1f
Jeremy (Jiaming) Chai <jiamingchai@yahoo.com>
2019-01-29 22:53:38 -0500

    sample project

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3 as sql
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Second Commit'
+host = 'http://127.0.0.1:5000/'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/', methods=['POST', 'GET'])
+def name():
+    error = None
+    if request.method == 'POST':
+        result = valid_name(request.form['firstname'], request.form['lastname'])
+        if result:
+            return render_template('input.html', error=error, url=host, result=result)
+        else:
+            error = 'invalid user input'
+    return render_template('input.html', error=error, url=host)
+
+
+def valid_name(firstname, lastname):
+    connection = sql.connect('database.db')
+    connection.execute('INSERT INTO users (firstname, lastname) VALUES (?,?);', (firstname, lastname))
+    connection.commit()
+    cursor = connection.execute('SELECT * FROM users;')
+    return cursor.fetchall()
--- readme.md
+++
@@ -1,1 +0,0 @@
-testing with commit & push
